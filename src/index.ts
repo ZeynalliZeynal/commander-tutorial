666bcbe24af52e1fd5ab8f1ee2dafffe3ba317f8
Zeynalli Zeynal <zzeynalli446@gmail.com>
2025-01-29 06:12:04 +0400

    add commands in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
 #!/usr/bin/env node
 
-const hello = () => {};
+import chalk from "chalk";
+import { Command } from "commander";
 
-console.log("yes");
+// declare the program
+const program = new Command();
+
+// add action to the program
+// program
+//   .description("Welcome!")
+// .argument("<name>", "Your name")
+//   .action((name: string) => {
+//     console.log(chalk.bgYellow.bold.black(`Hello ${name}`));
+//   });
+
+program
+  .command("init")
+  .description("Initialize the library and packages.")
+  .option("-y, --yes", "Skip the prompts and use the default values.")
+  .action((opts) => {
+    console.log(chalk.gray(opts.yes));
+    console.log(chalk.greenBright("Initializing the library and packages."));
+  });
+
+program
+  .command("add <numbers...>")
+  .action((numbers: number[]) => {
+    console.log(numbers.reduce((acc, curr) => acc + curr, 0));
+  })
+  .description("Add numbers");
+
+// execute the program with the given arguments
+program.parse(process.argv);
